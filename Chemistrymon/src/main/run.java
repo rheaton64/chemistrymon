7663621d624d06c0fb222342f2f4d0c27418d070
Ryan <ryguyheaton@gmail.com>
2019-05-19 16:38:28 -0400

    done most of the logic, need to bug fix and paste in questions

--- Chemistrymon/src/main/run.java
+++ Chemistrymon/src/main/run.java
@@ -1,12 +1,17 @@
 package main;
 
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -17,7 +22,20 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+
+
 public class run extends Application{
+	
+	String[] questions = {"test 1", "test 2", "test 3", "test 4", "test 5", "test 6"};
+	String[] answers = {"a", "a", "a", "a", "a", "a", "a"};
+	String[] difficulty = {"easy", "hard", "easy", "hard", "easy", "hard"};
+	ArrayList<Integer> answered;
+	int qInUse = 0;
+	int p1HealthValue = 20;
+	int p2HealthValue = 20;
+	boolean p1Turn = true;
+	Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -36,6 +54,14 @@ public class run extends Application{
 		tf = new TextField();
 		tf.setPrefSize(500, 30);
 		tf.setFont(Font.font("Helvetica", 35));
+		
+		tf.setOnAction(value -> {
+			if(tf.getText().equals("!reset")) {
+				resetGame();
+			} else {
+			answerSubmitted(tf.getText());
+			}
+		});
 		
 		ann = new Label("The attack was super effective!");
 		ann.setWrapText(true);
@@ -113,6 +139,154 @@ public class run extends Application{
 		Scene scene = new Scene(gui, 1920, 1080);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Chemistry Battle Simulator");
+		
+		
 		primaryStage.show();
+		resetGame();
+	}
+
+	public void startQuestions() {
+		answered = new ArrayList<Integer>();
+		newQuestion();
+	}
+	
+	public int newRandom() {
+		Random rand = new Random();
+		int gen = rand.nextInt(questions.length);
+		return gen;
+	}
+	
+	public void newQuestion() {
+		
+		int gen = 0;
+		
+		while(true) {
+			gen = newRandom();
+			if(!hasBeenUsed(gen)) {
+				break;
+			}
+			
+			if(answered.size() == questions.length) {
+				answered.clear();
+			}
+			
+		}
+		
+		question.setText(questions[gen]);
+		qInUse = gen;
+	}
+	
+	public boolean hasBeenUsed(int gen) {
+		if(answered.contains(gen)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void answerSubmitted(String a) {
+		
+		answered.add(qInUse);
+		
+		boolean isSuperEffective;
+		
+		if(a.equalsIgnoreCase(answers[qInUse])) {
+			if(p1Turn) {
+				isSuperEffective = p2Damage(difficulty[qInUse]);
+			} else {
+				isSuperEffective = p1Damage(difficulty[qInUse]);
+			}
+			
+			if(isSuperEffective) {
+				ann.setText("Wow! Super effective!");
+			} else {
+				ann.setText("Good attack!");
+			}
+			
+		} else {
+			ann.setText("Oh no! The attack missed!");
+		}
+		
+
+		
+	}
+	
+	public boolean p1Damage(String diff) {
+		if(diff.equals("easy")) {
+			for(int i = 0; i < 5; i++) {
+				p1HealthArray[p1HealthValue - 1].setFill(Color.WHITE);
+				p1HealthValue--;
+				if(p1HealthValue == 0) {
+					break;
+				}
+			}
+			return false;
+		} else {
+			for(int i = 0; i < 10; i++) {
+				p1HealthArray[p1HealthValue - 1].setFill(Color.WHITE);
+				p1HealthValue--;
+				if(p1HealthValue == 0) {
+					break;
+				}
+			}
+			return true;
+		}
+	}
+	
+	public boolean p2Damage(String diff) {
+		if(diff.equals("easy")) {
+			for(int i = 0; i < 5; i++) {
+				p2HealthArray[p2HealthValue - 1].setFill(Color.WHITE);
+				p2HealthValue--;
+				if(p2HealthValue == 0) {
+					break;
+				}
+			}
+			return false;
+		} else {
+			for(int i = 0; i < 10; i++) {
+				p2HealthArray[p2HealthValue - 1].setFill(Color.WHITE);
+				p2HealthValue--;
+				if(p2HealthValue == 0) {
+					break;
+				}
+			}
+			return true;
+		}
+	}
+	
+	public void finishTurn() throws InterruptedException {
+		if(p1Turn) {
+			if(p2HealthValue == 0) {
+				ann.setText("Congrats! " + p1Name.getText() + " wins!");
+			} else {
+				Thread.sleep(1000);
+				ann.setText("" + p2Name.getText() + ", it's your turn");
+				p1Turn = false;
+				newQuestion();
+			}
+		} else {
+			if(p1HealthValue == 0) {
+				ann.setText("Congrats! " + p2Name.getText() + " wins!");
+			} else {
+				Thread.sleep(1000);
+				ann.setText("" + p1Name.getText() + ", it's your turn");
+				p1Turn = true;
+				newQuestion();
+			}
+		}
+	}
+	
+	public void resetGame() {
+		p1HealthValue = 20;
+		p2HealthValue = 20;
+		///for(int i = 0; i < 20; i++) {
+		//	p1HealthArray[i].setFill(Color.RED);
+		//	p2HealthArray[i].setFill(Color.RED);
+		//}
+		p1Name.setText(scan.next());
+		p2Name.setText(scan.next());
+		p1Turn = true;
+		startQuestions();
+		
 	}
 }
